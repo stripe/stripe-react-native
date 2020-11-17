9a4728673ab607b3bfedb270e84496851b8a9cb1
Callstack <jakub.klobus@outlook.com>
2020-11-17 13:20:30 +0100

    feat: basic Stripe dependency setup

--- ios/StripeSdk.swift
+++ ios/StripeSdk.swift
@@ -3,6 +3,8 @@ class StripeSdk: NSObject {
 
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
+        resolve("a*b")
+//        TODO: below line is just a test of Stripe usage
+        Stripe.setDefaultPublishableKey("publishableKey")
     }
 }
