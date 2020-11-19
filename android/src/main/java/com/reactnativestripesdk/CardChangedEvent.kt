dab50dd72b1ff66df577876e8f8a172db55876e8
Arkadiusz Kubaczkowski <arek.kubaczkowski@gmail.com>
2020-11-19 14:29:31 +0100

    feat: android card field props (#21)
    * chore: add onChange prop
    
    * fix: docs
    
    * chore: add onFocus props
    
    * chore: add value prop
    
    Co-authored-by: Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>

---
+++ android/src/main/java/com/reactnativestripesdk/CardChangedEvent.kt
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package com.reactnativestripesdk
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.WritableNativeMap
+import com.facebook.react.uimanager.events.Event
+import com.facebook.react.uimanager.events.RCTEventEmitter
+
+internal class CardChangedEvent constructor(viewTag: Int, private val cardDetails: MutableMap<String, String>) : Event<CardChangedEvent>(viewTag) {
+  override fun getEventName(): String {
+    return EVENT_NAME
+  }
+
+  override fun dispatch(rctEventEmitter: RCTEventEmitter) {
+    rctEventEmitter.receiveEvent(viewTag, eventName, serializeEventData())
+  }
+
+  private fun serializeEventData(): WritableMap {
+    val eventData = Arguments.createMap()
+    for ((key, value) in cardDetails.entries) {
+      eventData.putString(key, value)
+    }
+
+    return eventData
+  }
+
+  companion object {
+    const val EVENT_NAME = "onCardChange"
+  }
+
+}
--- android/src/main/java/com/reactnativestripesdk/StripeSdkView.kt
+++ android/src/main/java/com/reactnativestripesdk/StripeSdkView.kt
@@ -1,24 +1,97 @@
 package com.reactnativestripesdk
 
-import android.R
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.common.MapBuilder
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
+import com.facebook.react.uimanager.UIManagerModule
 import com.facebook.react.uimanager.annotations.ReactProp
+import com.facebook.react.uimanager.events.EventDispatcher
+import com.stripe.android.view.CardInputListener
 import com.stripe.android.view.CardInputWidget
+
 
 class StripeSdkView : SimpleViewManager<CardInputWidget>() {
 
   override fun getName() = "CardField"
 
-  @ReactProp(name = "postalCodeEnabled")
-  fun setPostalCodeEnabled(view: CardInputWidget, enabled: Boolean?) {
-      if (enabled == null) return
-      view.postalCodeEnabled = enabled
+  private var inputWidget: CardInputWidget? = null
+
+  private lateinit var mEventDispatcher: EventDispatcher
+
+  private val cardDetails: MutableMap<String, String> = mutableMapOf("cardNumber" to "", "cvc" to "", "expiryDate" to "")
+
+  @ReactProp(name = "value")
+  fun setValue(view: CardInputWidget, value: ReadableMap) {
+    if (value == null) return
+    view.setCardNumber(value.getString("cardNumber"))
+    view.setCardNumber(value.getString("cvc"))
+    view.setCardNumber(value.getString("expiryDate"))
+  }
+
+  fun onCardChanged() {
+    Log.d("cardDetails", "onCardChanged: " + cardDetails.toString())
+    if (mEventDispatcher != null) {
+      mEventDispatcher.dispatchEvent(
+        CardChangedEvent(inputWidget!!.id, cardDetails))
+    }
+  }
+
+  override fun getExportedCustomDirectEventTypeConstants(): MutableMap<String, Any> {
+    return MapBuilder.of(
+      CardFocusEvent.EVENT_NAME, MapBuilder.of("registrationName", "onFocus"),
+      CardChangedEvent.EVENT_NAME, MapBuilder.of("registrationName", "onCardChange"))
+  }
+
+
+  private fun setListeners() {
+    inputWidget?.setCardInputListener(object :CardInputListener{
+      override fun onPostalCodeComplete() {}
+      override fun onCardComplete() {}
+      override fun onExpirationComplete() {}
+      override fun onCvcComplete() {}
+      override fun onFocusChange(focusField: String?) {
+        if (mEventDispatcher != null) {
+          mEventDispatcher.dispatchEvent(
+            CardFocusEvent(inputWidget!!.id, focusField))
+        }
+      }
+    })
+    inputWidget?.setCardNumberTextWatcher(object :TextWatcher{
+      override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+      override fun afterTextChanged(p0: Editable?) { }
+      override fun onTextChanged(var1: CharSequence?, var2: Int, var3: Int, var4: Int) {
+        cardDetails["cardNumber"] = var1.toString()
+        onCardChanged()
+      }
+    })
+    inputWidget?.setCvcNumberTextWatcher(object :TextWatcher{
+      override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+      override fun afterTextChanged(p0: Editable?) { }
+      override fun onTextChanged(var1: CharSequence?, var2: Int, var3: Int, var4: Int) {
+        cardDetails["cvc"] = var1.toString()
+        onCardChanged()
+      }
+    })
+    inputWidget?.setExpiryDateTextWatcher(object :TextWatcher{
+      override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+      override fun afterTextChanged(p0: Editable?) { }
+      override fun onTextChanged(var1: CharSequence?, var2: Int, var3: Int, var4: Int) {
+        cardDetails["expiryDate"] = var1.toString()
+        onCardChanged()
+      }
+    })
   }
 
   override fun createViewInstance(reactContext: ThemedReactContext): CardInputWidget {
-    val inputWidget = CardInputWidget(reactContext)
+    mEventDispatcher = reactContext.getNativeModule(UIManagerModule::class.java).eventDispatcher
 
-    return inputWidget
+    inputWidget = CardInputWidget(reactContext)
+    setListeners()
+
+    return inputWidget as CardInputWidget
   }
 }
