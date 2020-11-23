081c87ea29c404797285bdceb6c6b06a20233753
Jakub Kłobus <souhepend@hotmail.com>
2020-11-23 11:30:10 +0100

    feat: confirm payment for ios (#26)
    * feat: stript provider for iOS
    
    * feat: initialise function for android
    
    * feat: fetching publishable key from API
    
    * feat: confirm-payment basics
    
    * feat: example improvements
    
    * feat: almost successful basic payment
    
    * feat: basic succes and error handlers
    
    * feat: confirm payment callbacks
    
    * feat: promise support
    
    * feat: loading support
    
    * feat: basic swift implementation
    
    * feat: confirmPayment for iOS

--- ios/StripeSdk.swift
+++ ios/StripeSdk.swift
@@ -1,9 +1,74 @@
 @objc(StripeSdk)
 class StripeSdk: NSObject {
+    
+    var onPaymentSuccessCallback: RCTResponseSenderBlock? = nil
+    var onPaymentErrorCallback: RCTResponseSenderBlock? = nil
 
     @objc(initialise:)
     func initialise(publishableKey: String) -> Void {
         Stripe.setDefaultPublishableKey(publishableKey)
     }
+    
+    @objc(registerConfirmPaymentCallbacks:onError:)
+    func registerConfirmPaymentCallbacks(onSuccess: @escaping RCTResponseSenderBlock, onError: @escaping RCTResponseSenderBlock) -> Void  {
+        onPaymentSuccessCallback = onSuccess
+        onPaymentErrorCallback = onError
+    }
+    
+    @objc(confirmPaymentMethod:params:resolver:rejecter:)
+    func confirmPaymentMethod(
+        paymentIntentClientSecret: String,
+        params: NSDictionary,
+        resolver resolve: @escaping RCTPromiseResolveBlock,
+        rejecter reject: @escaping RCTPromiseRejectBlock
+    ) -> Void {
+        let cardSourceParams = STPCardParams()
+        cardSourceParams.number = RCTConvert.nsString(params["cardNumber"])
+        cardSourceParams.cvc = RCTConvert.nsString(params["cvc"])
+        cardSourceParams.expMonth = RCTConvert.nsuInteger(params["expiryMonth"])
+        cardSourceParams.expYear = RCTConvert.nsuInteger(params["expiryYear"])
+        
+        let cardParams = STPPaymentMethodCardParams(cardSourceParams: cardSourceParams)
+        let paymentMethodParams = STPPaymentMethodParams(card: cardParams, billingDetails: nil, metadata: nil)
+        let paymentIntentParams = STPPaymentIntentParams(clientSecret: paymentIntentClientSecret)
+        paymentIntentParams.paymentMethodParams = paymentMethodParams
 
+        let paymentHandler = STPPaymentHandler.shared()
+        paymentHandler.confirmPayment(withParams: paymentIntentParams, authenticationContext: self) { (status, paymentIntent, error) in
+            switch (status) {
+            case .failed:
+                reject("PAYMENT_FAILED", error?.localizedDescription ?? "", nil)
+                self.onPaymentErrorCallback?([NSNull(), error?.localizedDescription ?? ""])
+                break
+            case .canceled:
+                reject("PAYMENT_CANCELED", error?.localizedDescription ?? "", nil)
+                self.onPaymentErrorCallback?([NSNull(), error?.localizedDescription ?? ""])
+                break
+            case .succeeded:
+                let intent: NSDictionary = [
+                    "id": paymentIntent?.paymentMethodId ?? "",
+                    "currency": paymentIntent?.currency ?? "",
+                    "status": paymentIntent?.status.rawValue ?? "",
+                    "description": paymentIntent?.description ?? "",
+                    "receiptEmail": paymentIntent?.receiptEmail ?? "",
+                ]
+                resolve(intent)
+                self.onPaymentSuccessCallback?([NSNull(), intent])
+                break
+            @unknown default:
+                reject("PAYMENT_ERROR", "Cannot complete payment", nil)
+                self.onPaymentErrorCallback?([NSNull(), "Cannot complete payment"])
+                break
+            }
+        }
+    }
+}
+
+extension StripeSdk: STPAuthenticationContext {
+    func authenticationPresentingViewController() -> UIViewController {
+        if let topViewController = UIApplication.shared.delegate?.window??.rootViewController {
+            return topViewController
+        }
+        return UIViewController()
+    }
 }
--- src/NativeStripeSdk.tsx
+++ src/NativeStripeSdk.tsx
@@ -8,8 +8,12 @@ type NativeStripeSdkType = {
     cardDetails: CardDetails
   ): Promise<Intent>;
   registerConfirmPaymentCallbacks(
-    onSuccess: (intent: Intent) => void,
-    onError: (errorMessage: string) => void
+    onSuccess:
+      | ((intent: Intent) => void)
+      | ((error: any, intent: Intent) => void),
+    onError:
+      | ((errorMessage: string) => void)
+      | ((error: any, errorMessage: string) => void)
   ): void;
 };
 
