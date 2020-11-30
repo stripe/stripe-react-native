b9fed64df30e0d991e115d1a7efad46259f65928
Arkadiusz Kubaczkowski <arek.kubaczkowski@gmail.com>
2020-11-30 12:37:49 +0100

    feat: setup intent payments for iOS (#66)
    * chore: confirm payment intent on ios
    
    * chore: add address to billing
    
    * fix: setting billing address
    
    * chore: display error code
    
    * chore: create saparate class for errors
    
    Co-authored-by: souhe <jakub.klobus@outlook.com>
    Co-authored-by: Jakub Kłobus <souhepend@hotmail.com>
    Co-authored-by: Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>

---
+++ ios/Errors.swift
@@ -0,0 +1,26 @@
+enum ConfirmPaymentErrorType: String {
+    case Failed, Canceled, Unknown
+}
+
+enum ApplePayErrorType: String {
+    case Failed, Canceled, Unknown
+}
+
+enum NextPaymentActionErrorType: String {
+    case Failed, Canceled, Unknown
+}
+
+enum ConfirmSetupIntentErrorType: String {
+    case Failed, Canceled, Unknown
+}
+
+class Errors {
+    class func createError (code: String, message: String) -> NSDictionary {
+        let error: NSDictionary = [
+            "code": code,
+            "message": message,
+        ]
+        
+        return error
+    }
+}
--- ios/Mappers.swift
+++ ios/Mappers.swift
@@ -1,43 +1,32 @@
-enum ConfirmPaymentErrorType: String {
-    case Failed, Canceled, Unknown
-}
-
-enum ApplePayErrorType: String {
-    case Failed, Canceled, Unknown
-}
-
-enum NextPaymentActionErrorType: String {
-    case Failed, Canceled, Unknown
-}
 
 class Mappers {
     class func mapIntentStatus(status: STPPaymentIntentStatus?) -> String {
         if let status = status {
             switch status {
-                case STPPaymentIntentStatus.succeeded: return "Succeeded"
-                case STPPaymentIntentStatus.requiresPaymentMethod: return "RequiresPaymentMethod"
-                case STPPaymentIntentStatus.requiresConfirmation: return "RequiresConfirmation"
-                case STPPaymentIntentStatus.canceled: return "Canceled"
-                case STPPaymentIntentStatus.processing: return "Processing"
-                case STPPaymentIntentStatus.requiresAction: return "RequiresAction"
-                case STPPaymentIntentStatus.requiresCapture: return "RequiresCapture"
-                default: return "Unknown"
+            case STPPaymentIntentStatus.succeeded: return "Succeeded"
+            case STPPaymentIntentStatus.requiresPaymentMethod: return "RequiresPaymentMethod"
+            case STPPaymentIntentStatus.requiresConfirmation: return "RequiresConfirmation"
+            case STPPaymentIntentStatus.canceled: return "Canceled"
+            case STPPaymentIntentStatus.processing: return "Processing"
+            case STPPaymentIntentStatus.requiresAction: return "RequiresAction"
+            case STPPaymentIntentStatus.requiresCapture: return "RequiresCapture"
+            default: return "Unknown"
             }
         }
         return "Unknown"
     }
     
-    class func mapCardParams(params: NSDictionary) -> STPPaymentMethodParams {
+    class func mapCardParamsToPaymentMethodParams(params: NSDictionary) -> STPPaymentMethodParams {
         let cardSourceParams = STPCardParams()
         cardSourceParams.number = RCTConvert.nsString(params["cardNumber"])
         cardSourceParams.cvc = RCTConvert.nsString(params["cvc"])
         cardSourceParams.expMonth = RCTConvert.nsuInteger(params["expiryMonth"])
         cardSourceParams.expYear = RCTConvert.nsuInteger(params["expiryYear"])
-          
+        
         let cardParams = STPPaymentMethodCardParams(cardSourceParams: cardSourceParams)
         return STPPaymentMethodParams(card: cardParams, billingDetails: nil, metadata: nil)
     }
-      
+    
     class func mapFromIntent (paymentIntent: STPPaymentIntent?) -> NSDictionary {
         let intent: NSDictionary = [
             "id": paymentIntent?.paymentMethodId ?? "",
@@ -47,16 +36,66 @@ class Mappers {
             "receiptEmail": paymentIntent?.receiptEmail ?? "",
             "stripeId": paymentIntent?.stripeId ?? "",
         ]
-          
+        
         return intent;
     }
+
+    class func mapToBillingDetails(billingDetails: NSDictionary) -> STPPaymentMethodBillingDetails {
+        let billing = STPPaymentMethodBillingDetails()
+        billing.email = RCTConvert.nsString(billingDetails["email"])
+        billing.phone = RCTConvert.nsString(billingDetails["phone"])
+        billing.name = RCTConvert.nsString(billingDetails["name"])
+        
+        let billingAddres = STPPaymentMethodAddress()
+
+        billingAddres.city = RCTConvert.nsString(billingDetails["addressCity"])
+        billingAddres.postalCode = RCTConvert.nsString(billingDetails["addressPostalCode"])
+        billingAddres.country = RCTConvert.nsString(billingDetails["addressCountry"])
+        billingAddres.line1 = RCTConvert.nsString(billingDetails["addressLine1"])
+        billingAddres.line2 = RCTConvert.nsString(billingDetails["addressLine2"])
+        billingAddres.state = RCTConvert.nsString(billingDetails["addressState"])
+        
+        billing.address = billingAddres
+        
+        return billing
+    }
     
-    class func createError (code: String, message: String) -> NSDictionary {
-        let error: NSDictionary = [
-            "code": code,
-            "message": message,
+    class func mapCardParams(params: NSDictionary) -> STPPaymentMethodCardParams {
+        let cardSourceParams = STPCardParams()
+        cardSourceParams.number = RCTConvert.nsString(params["cardNumber"])
+        cardSourceParams.cvc = RCTConvert.nsString(params["cvc"])
+        cardSourceParams.expMonth = RCTConvert.nsuInteger(params["expiryMonth"])
+        cardSourceParams.expYear = RCTConvert.nsuInteger(params["expiryYear"])
+        
+        return STPPaymentMethodCardParams(cardSourceParams: cardSourceParams)
+    }
+    
+    class func mapIntentStatus(status: STPSetupIntentStatus?) -> String {
+        if let status = status {
+            switch status {
+            case STPSetupIntentStatus.succeeded: return "Succeeded"
+            case STPSetupIntentStatus.requiresPaymentMethod: return "RequiresPaymentMethod"
+            case STPSetupIntentStatus.requiresConfirmation: return "RequiresConfirmation"
+            case STPSetupIntentStatus.canceled: return "Canceled"
+            case STPSetupIntentStatus.processing: return "Processing"
+            case STPSetupIntentStatus.requiresAction: return "RequiresAction"
+            case STPSetupIntentStatus.unknown: return "Unknown"
+            default: return "Unknown"
+            }
+        }
+        return "Unknown"
+        
+    }
+    
+    class func mapFromSetupIntentResult(setupIntent: STPSetupIntent) -> NSDictionary {
+        let intent: NSDictionary = [
+            "id": setupIntent.stripeID,
+            "status": mapIntentStatus(status: setupIntent.status),
+            "description": setupIntent.stripeDescription ?? "",
+            "created": Int(setupIntent.created.timeIntervalSince1970 * 1000) // convert to unix timestamp
         ]
         
-        return error
+        return intent
     }
+    
 }
