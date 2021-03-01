05ce2b9e98f8de47da46cb83e056c0a6bc0967f4
Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>
2021-03-01 17:02:06 +0100

    chore: documentation

--- example/src/screens/HomeScreen.tsx
+++ example/src/screens/HomeScreen.tsx
@@ -48,7 +48,7 @@ export default function HomeScreen() {
         <Button
           title="Re-collect CVC"
           onPress={() => {
-            navigation.navigate('ReCollectCVCScreen');
+            navigation.navigate('CVCReCollectionScreen');
           }}
         />
       </View>
