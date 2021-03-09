a304f44dae47b0161dea42aa9c8c07c7b9329b2b
Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>
2021-03-09 11:51:01 +0100

    chore: docs, improvements

--- src/hooks/useApplePay.tsx
+++ src/hooks/useApplePay.tsx
@@ -1,4 +1,4 @@
-import type { ApplePay } from '../types';
+import type { ApplePay, ApplePayError, StripeError } from '../types';
 import { useEffect, useState } from 'react';
 import { useStripe } from './useStripe';
 import { NativeEventEmitter, NativeModules } from 'react-native';
@@ -6,42 +6,88 @@ import { NativeEventEmitter, NativeModules } from 'react-native';
 const eventEmitter = new NativeEventEmitter(NativeModules.StripeSdk);
 
 export interface Props {
+  /**
+   *
+   * @example
+   * ```ts
+   * const {} = useApplePay({
+   *  onDidSetShippingMethodCallback: (shippingMethod, handler) => {
+   *    handler([
+   *      { label: 'Example item name 1', amount: '11.00' },
+   *      { label: 'Example item name 2', amount: '25.00' },
+   *   ]);
+   *  }
+   * })
+   * ```
+   */
   onDidSetShippingContactCallback?: (
-    shippingMethod: ApplePay.ShippingMethod
+    shippingMethod: ApplePay.ShippingMethod,
+    handler: (
+      summaryItems: ApplePay.CartSummaryItem[]
+    ) => Promise<{
+      error?: StripeError<ApplePayError>;
+    }>
   ) => void;
+  /**
+   *
+   * @example
+   * ```ts
+   * const {} = useApplePay({
+   *  onDidSetShippingMethodCallback: (shippingContact, handler) => {
+   *    handler([
+   *      { label: 'Example item name 1', amount: '11.00' },
+   *      { label: 'Example item name 2', amount: '25.00' },
+   *   ]);
+   *  }
+   * })
+   * ```
+   */
   onDidSetShippingMethodCallback?: (
-    shippingContact: ApplePay.ShippingContact
+    shippingContact: ApplePay.ShippingContact,
+    handler: (
+      summaryItems: ApplePay.CartSummaryItem[]
+    ) => Promise<{
+      error?: StripeError<ApplePayError>;
+    }>
   ) => void;
 }
+
+const SET_SHIPPING_METHOD_CALLBACK_NAME = 'onDidSetShippingMethod';
+const SET_SHIPPING_CONTACT_CALLBACK_NAME = 'onDidSetShippingContact';
 
 /**
  * useApplePay hook
  */
 export function useApplePay({
-  onDidSetShippingContactCallback = () => {},
-  onDidSetShippingMethodCallback = () => {},
+  onDidSetShippingContactCallback,
+  onDidSetShippingMethodCallback,
 }: Props = {}) {
   const {
     isApplePaySupported,
     presentApplePay: presentApplePayNative,
     confirmApplePayPayment: confirmApplePayPaymentNative,
+    updateApplePaySummaryItems,
   } = useStripe();
+  const [items, setItems] = useState<ApplePay.CartSummaryItem[] | null>(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    eventEmitter.addListener('onDidSetShippingMethod', onDidSetShippingMethod);
     eventEmitter.addListener(
-      'onDidSetShippingContact',
+      SET_SHIPPING_METHOD_CALLBACK_NAME,
+      onDidSetShippingMethod
+    );
+    eventEmitter.addListener(
+      SET_SHIPPING_CONTACT_CALLBACK_NAME,
       onDidSetShippingContact
     );
 
     return () => {
       eventEmitter.removeListener(
-        'onDidSetShippingMethod',
+        SET_SHIPPING_METHOD_CALLBACK_NAME,
         onDidSetShippingMethod
       );
       eventEmitter.removeListener(
-        'onDidSetShippingContact',
+        SET_SHIPPING_CONTACT_CALLBACK_NAME,
         onDidSetShippingMethod
       );
     };
@@ -49,16 +95,34 @@ export function useApplePay({
   }, []);
 
   const onDidSetShippingMethod = (shippingMethod: any) => {
-    onDidSetShippingMethodCallback(shippingMethod);
+    if (onDidSetShippingMethodCallback) {
+      onDidSetShippingMethodCallback(
+        shippingMethod,
+        updateApplePaySummaryItems
+      );
+    } else {
+      updateApplePaySummaryItems(items as ApplePay.CartSummaryItem[]);
+    }
   };
 
   const onDidSetShippingContact = (shippingContact: any) => {
-    onDidSetShippingContactCallback(shippingContact);
+    if (onDidSetShippingContactCallback) {
+      onDidSetShippingContactCallback(
+        shippingContact,
+        updateApplePaySummaryItems
+      );
+    } else {
+      updateApplePaySummaryItems(items as ApplePay.CartSummaryItem[]);
+    }
   };
 
-  const presentApplePay = async (data: ApplePay.PresentParams) => {
+  const presentApplePay = async (params: ApplePay.PresentParams) => {
     setLoading(true);
-    const result = await presentApplePayNative(data);
+    setItems(params.cartItems);
+    const result = await presentApplePayNative(params);
+    if (result.error) {
+      setItems(null);
+    }
     setLoading(false);
     return result;
   };
@@ -66,6 +130,7 @@ export function useApplePay({
   const confirmApplePayPayment = async (clientSecret: string) => {
     setLoading(true);
     const result = await confirmApplePayPaymentNative(clientSecret);
+    setItems(null);
     setLoading(false);
     return result;
   };
