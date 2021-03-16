68c399a15b0cde82a4434489b469e878400a166d
Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>
2021-03-16 07:56:00 +0100

    chore: detox configuration and smoke test

---
+++ example/e2e/synchronousPayment.e2e.js
@@ -0,0 +1,34 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should pay synchronously ', async () => {
+    await element(by.text('Card payment without webhooks')).tap();
+    await expect(element(by.text('Pay'))).toBeVisible();
+    if (device.getPlatform() === 'ios') {
+      await element(by.label('card number')).typeText('4242424242424242');
+      await element(by.label('expiration date')).typeText('12/22');
+      await element(by.label('CVC')).typeText('123');
+    } else {
+      await element(by.type('android.widget.EditText'))
+        .atIndex(0)
+        .typeText('4242424242424242');
+      await element(by.type('android.widget.EditText'))
+        .atIndex(1)
+        .typeText('12/22');
+      await element(by.type('android.widget.EditText'))
+        .atIndex(2)
+        .typeText('123');
+    }
+
+    await element(by.text('Pay')).tap();
+    await waitFor(element(by.text('Success')))
+      .toBeVisible()
+      .withTimeout(5000);
+  });
+});
