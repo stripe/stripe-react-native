efb9cda6d890e7fcf5d9b318e2970608348bd0e7
Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>
2021-04-09 09:45:57 +0200

    chore: tmp

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -45,113 +45,124 @@ export default function App() {
   }, []);
 
   return (
-    <StripeProvider
-      publishableKey={publishableKey}
-      merchantIdentifier="merchant.com.stripe.react.native"
-      urlScheme="stripe-example"
-      threeDSecureParams={{
-        backgroundColor: colors.white,
-        submitButton: {
-          textColor: colors.white,
-          textFontSize: 12,
-        },
-      }}
-    >
-      <StatusBar
-        backgroundColor={colors.blurple_dark}
-        barStyle="light-content"
-        translucent
-      />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerTintColor: colors.white,
-            headerStyle: {
-              shadowOpacity: 0,
-              backgroundColor: colors.blurple,
-              borderBottomWidth: StyleSheet.hairlineWidth,
-              borderBottomColor: colors.slate,
-            },
-            headerTitleStyle: {
-              color: colors.white,
-            },
-            headerBackTitleStyle: {
-              color: colors.white,
-            },
-          }}
-        >
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen
-            name="WebhookPayment"
-            component={WebhookPaymentScreen}
-          />
-          <Stack.Screen
-            name="NoWebhookPayment"
-            component={NoWebhookPaymentScreen}
-          />
-          <Stack.Screen name="ApplePay" component={ApplePayScreen} />
-          <Stack.Screen
-            name="SetupFuturePayment"
-            component={SetupFuturePaymentScreen}
-          />
-          <Stack.Screen
-            name="CVCReCollectionScreen"
-            component={CVCReCollectionScreen}
-          />
-          <Stack.Screen name="IdealPayment" component={IdealPaymentScreen} />
-          <Stack.Screen
-            name="IdealSetupFuturePaymentScreen"
-            component={IdealSetupFuturePaymentScreen}
-          />
-          <Stack.Screen
-            name="AlipayPaymentScreen"
-            component={AlipayPaymentScreen}
-          />
-          <Stack.Screen name="P24PaymentScreen" component={P24PaymentScreen} />
-          <Stack.Screen
-            name="PaymentResultScreen"
-            component={PaymentResultScreen}
-          />
-          <Stack.Screen name="FPXPaymentScreen" component={FPXPaymentScreen} />
-          <Stack.Screen
-            name="SofortPaymentScreen"
-            component={SofortPaymentScreen}
-          />
-          <Stack.Screen
-            name="SofortSetupFuturePaymentScreen"
-            component={SofortSetupFuturePaymentScreen}
-          />
-          <Stack.Screen
-            name="GrabPayPaymentScreen"
-            component={GrabPayPaymentScreen}
-          />
-          <Stack.Screen
-            name="BancontactPaymentScreen"
-            component={BancontactPaymentScreen}
-          />
-          <Stack.Screen name="EPSPaymentScreen" component={EPSPaymentScreen} />
-          <Stack.Screen
-            name="BancontactSetupFuturePaymentScreen"
-            component={BancontactSetupFuturePaymentScreen}
-          />
-          <Stack.Screen
-            name="SepaPaymentScreen"
-            component={SepaPaymentScreen}
-          />
-          <Stack.Screen
-            name="SepaSetupFuturePaymentScreen"
-            component={SepaSetupFuturePaymentScreen}
-          />
-          <Stack.Screen
-            name="OxxoPaymentScreen"
-            component={OxxoPaymentScreen}
-          />
-          <Stack.Screen
-            name="GiropayPaymentScreen"
-            component={GiropayPaymentScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </StripeProvider>
+    <View style={{ flex: 1 }} accessibilityLabel="app-root">
+      <StripeProvider
+        publishableKey={publishableKey}
+        merchantIdentifier="merchant.com.stripe.react.native"
+        urlScheme="stripe-example"
+        threeDSecureParams={{
+          backgroundColor: colors.white,
+          submitButton: {
+            textColor: colors.white,
+            textFontSize: 12,
+          },
+        }}
+      >
+        <StatusBar
+          backgroundColor={colors.blurple_dark}
+          barStyle="light-content"
+          translucent
+        />
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerTintColor: colors.white,
+              headerStyle: {
+                shadowOpacity: 0,
+                backgroundColor: colors.blurple,
+                borderBottomWidth: StyleSheet.hairlineWidth,
+                borderBottomColor: colors.slate,
+              },
+              headerTitleStyle: {
+                color: colors.white,
+              },
+              headerBackTitleStyle: {
+                color: colors.white,
+              },
+            }}
+          >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen
+              name="WebhookPayment"
+              component={WebhookPaymentScreen}
+            />
+            <Stack.Screen
+              name="NoWebhookPayment"
+              component={NoWebhookPaymentScreen}
+            />
+            <Stack.Screen name="ApplePay" component={ApplePayScreen} />
+            <Stack.Screen
+              name="SetupFuturePayment"
+              component={SetupFuturePaymentScreen}
+            />
+            <Stack.Screen
+              name="CVCReCollectionScreen"
+              component={CVCReCollectionScreen}
+            />
+            <Stack.Screen name="IdealPayment" component={IdealPaymentScreen} />
+            <Stack.Screen
+              name="IdealSetupFuturePaymentScreen"
+              component={IdealSetupFuturePaymentScreen}
+            />
+            <Stack.Screen
+              name="AlipayPaymentScreen"
+              component={AlipayPaymentScreen}
+            />
+            <Stack.Screen
+              name="P24PaymentScreen"
+              component={P24PaymentScreen}
+            />
+            <Stack.Screen
+              name="PaymentResultScreen"
+              component={PaymentResultScreen}
+            />
+            <Stack.Screen
+              name="FPXPaymentScreen"
+              component={FPXPaymentScreen}
+            />
+            <Stack.Screen
+              name="SofortPaymentScreen"
+              component={SofortPaymentScreen}
+            />
+            <Stack.Screen
+              name="SofortSetupFuturePaymentScreen"
+              component={SofortSetupFuturePaymentScreen}
+            />
+            <Stack.Screen
+              name="GrabPayPaymentScreen"
+              component={GrabPayPaymentScreen}
+            />
+            <Stack.Screen
+              name="BancontactPaymentScreen"
+              component={BancontactPaymentScreen}
+            />
+            <Stack.Screen
+              name="EPSPaymentScreen"
+              component={EPSPaymentScreen}
+            />
+            <Stack.Screen
+              name="BancontactSetupFuturePaymentScreen"
+              component={BancontactSetupFuturePaymentScreen}
+            />
+            <Stack.Screen
+              name="SepaPaymentScreen"
+              component={SepaPaymentScreen}
+            />
+            <Stack.Screen
+              name="SepaSetupFuturePaymentScreen"
+              component={SepaSetupFuturePaymentScreen}
+            />
+            <Stack.Screen
+              name="OxxoPaymentScreen"
+              component={OxxoPaymentScreen}
+            />
+            <Stack.Screen
+              name="GiropayPaymentScreen"
+              component={GiropayPaymentScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </StripeProvider>
+    </View>
   );
 }
--- wdio.conf.js
+++ wdio.conf.js
@@ -14,6 +14,7 @@ exports.config = {
       deviceName: 'Pixel_3a_API_30_x86',
       app: 'example/android/app/build/outputs/apk/debug/app-debug.apk',
       automationName: 'UiAutomator2',
+      chromedriverUseSystemExecutable: true,
     },
   ],
   logLevel: 'info',
