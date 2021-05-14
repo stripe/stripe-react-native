0b1f9ee8d0be9e14444d6432dde6eaad258378eb
Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>
2021-05-14 14:47:43 +0200

    chore: types refactor

--- src/types/Card.ts
+++ src/types/Card.ts
@@ -8,4 +8,42 @@ export namespace Card {
     | 'UnionPay'
     | 'Visa'
     | 'Unknown';
+
+  export type TokenType =
+    | 'Account'
+    | 'BankAccount'
+    | 'Card'
+    | 'CvcUpdate'
+    | 'Person'
+    | 'Pii';
+
+  export interface Token {
+    id: string;
+    created: number;
+    type: TokenType;
+    used: boolean;
+    livemode: boolean;
+    card: Params;
+    bankAccount?: BankAccount;
+  }
+
+  export interface BankAccount {
+    bankName: string;
+    accountHolderName: string;
+    accountHolderType: 'Individual' | 'Company';
+    currency: string;
+    country: string;
+    routingNumber: string;
+    status: 'Errored' | 'New' | 'Validated' | 'VerificationFailed' | 'Verified';
+  }
+
+  export interface Params {
+    country: string;
+    brand: Card.Brand;
+    currency: string;
+    expMonth: number;
+    expYear: number;
+    last4: string;
+    funding: string;
+  }
 }
