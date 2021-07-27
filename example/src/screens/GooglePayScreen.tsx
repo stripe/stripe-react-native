778a168c25cee0232763aad886a77ade0f0fa5d6
Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>
2021-07-27 13:48:29 +0200

    fix: init on mount only

--- example/src/screens/GooglePayScreen.tsx
+++ example/src/screens/GooglePayScreen.tsx
@@ -93,7 +93,8 @@ export default function GooglePayScreen() {
       setInitialized(true);
     }
     initialize();
-  }, [initGooglePay]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <PaymentScreen>
