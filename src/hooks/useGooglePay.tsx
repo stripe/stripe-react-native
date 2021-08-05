cf5b149b8cf40af8b8e225031f95ef9435c21792
Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>
2021-08-05 10:09:50 +0200

    fix: google pay type import

--- src/hooks/useGooglePay.tsx
+++ src/hooks/useGooglePay.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react';
-import type { GooglePay } from 'src/types';
+import type { GooglePay } from '../types';
 import { useStripe } from './useStripe';
 
 /**
