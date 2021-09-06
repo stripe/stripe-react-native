171535640f107a0e40976f17c66e483526d43ef2
Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>
2021-09-06 11:56:43 +0200

    chore: jest mock file

--- src/hooks/useConfirmSetupIntent.tsx
+++ src/hooks/useConfirmSetupIntent.tsx
@@ -7,9 +7,9 @@ import { useStripe } from './useStripe';
  */
 export function useConfirmSetupIntent() {
   const [loading, setLoading] = useState(false);
-  const { confirmSetupIntent: confirmSetupIntentNative } = useStripe();
+  const { confirmSetupIntent: confirmSetupIntent } = useStripe();
 
-  const confirmSetupIntent = useCallback(
+  const _confirmSetupIntent = useCallback(
     async (
       paymentIntentClientSecret: string,
       data: ConfirmSetupIntent.Params,
@@ -17,7 +17,7 @@ export function useConfirmSetupIntent() {
     ) => {
       setLoading(true);
 
-      const result = await confirmSetupIntentNative(
+      const result = await confirmSetupIntent(
         paymentIntentClientSecret,
         data,
         options
@@ -27,11 +27,11 @@ export function useConfirmSetupIntent() {
 
       return result;
     },
-    [confirmSetupIntentNative]
+    [confirmSetupIntent]
   );
 
   return {
-    confirmSetupIntent,
+    confirmSetupIntent: _confirmSetupIntent,
     loading,
   };
 }
