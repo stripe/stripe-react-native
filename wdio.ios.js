a844d38a8678635dcc5978e84b03c6a9a732939f
Nick Porter <porter@stripe.com>
2021-10-18 16:39:39 -0600

    Revert CI changes

--- wdio.ios.js
+++ wdio.ios.js
@@ -50,7 +50,7 @@ exports.config = {
       maxInstances: 1,
       browserName: '',
       appiumVersion: '1.20.2',
-      platformVersion: '14.5',
+      platformVersion: '14.4',
       platformName: 'iOS',
       deviceName: 'iPhone 12',
       app: 'example/ios/DerivedData/StripeSdkExample/Build/Products/Release-iphonesimulator/StripeSdkExample.app',
