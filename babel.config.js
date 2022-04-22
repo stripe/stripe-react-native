004ca542fb6691713be56fb7640becc4850c2625
Charles Cruzan <charliecruzan@stripe.com>
2022-04-22 16:43:21 -0400

    create custom babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    function ModifyPackageJsonImportForTranspilation() {
+      return {
+        visitor: {
+          ImportDeclaration(path, _state) {
+            const { node } = path;
+            if (node.source.value === '../../package.json') {
+              node.source.value = '../../../package.json';
+            }
+          },
+        },
+      };
+    },
+  ],
 };
