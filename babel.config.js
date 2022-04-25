97ee77e38f7592e1a894f4dd5088c56068dbd56f
charliecruzan-stripe <97612659+charliecruzan-stripe@users.noreply.github.com>
2022-04-25 10:11:17 -0400

    fix: create custom babel plugin for package.json imports in `src/` (#902)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    function ModifyPackageJsonImportForTranspilation() {
+      return {
+        visitor: {
+          ImportDeclaration(path) {
+            let importResource = path.node.source.value ?? '';
+            if (importResource.includes('../package.json')) {
+              importResource = '../' + importResource;
+            }
+          },
+        },
+      };
+    },
+  ],
 };
