d7824785ce422bde750c8ec80f7f624ab6772a61
Charles Cruzan <charliecruzan@stripe.com>
2022-06-01 14:17:30 -0400

    changelog and fix warnings

--- android/src/main/java/com/reactnativestripesdk/PaymentSheetFragment.kt
+++ android/src/main/java/com/reactnativestripesdk/PaymentSheetFragment.kt
@@ -1,7 +1,6 @@
 package com.reactnativestripesdk
 
 import android.content.Context
-import android.content.res.ColorStateList
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
