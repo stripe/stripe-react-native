37e9a50936fb6f1e2279cbb4cc76a81e2ec7ed11
charliecruzan-stripe <97612659+charliecruzan-stripe@users.noreply.github.com>
2022-08-23 12:19:02 -0400

    fix: CardField postalCodeEnabled doc block

--- src/components/CardField.tsx
+++ src/components/CardField.tsx
@@ -30,7 +30,7 @@ const CardFieldNative =
  */
 export interface Props extends AccessibilityProps {
   style?: StyleProp<ViewStyle>;
-  /** Controls if a postal code entry field can be displayed to the user. Defaults to false. If true, the type of code entry shown is controlled by the set countryCode prop. Some country codes may result in no postal code entry being shown if those countries do not commonly use postal codes. If false, no postal code entry will ever be displayed. */
+  /** Controls if a postal code entry field can be displayed to the user. If true, the type of code entry shown is controlled by the set countryCode prop. Some country codes may result in no postal code entry being shown if those countries do not commonly use postal codes. If false, no postal code entry will ever be displayed. Defaults to true.*/
   postalCodeEnabled?: boolean;
   /** Controls the postal code entry shown (if the postalCodeEnabled prop is set to true). Defaults to the device's default locale. */
   countryCode?: string;
