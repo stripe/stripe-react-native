13e2e9e6ca6d4193e34391f8ff88eea6b499edff
Charles Cruzan <charliecruzan@stripe.com>
2022-10-06 12:57:35 -0700

    move file

--- ios/PaymentPassFinder.swift
+++ ios/PaymentPassFinder.swift
@@ -13,7 +13,7 @@ internal class PaymentPassFinder: NSObject, WCSessionDelegate {
         case CURRENT_DEVICE
         case PAIRED_DEVICE
     }
-    
+
     private var last4: String
     private var findPassOnWatchCompletion: ((Bool, [PassLocation]) -> Void)
     private var isPassOnCurrentDevice: Bool = false
@@ -22,7 +22,7 @@ internal class PaymentPassFinder: NSObject, WCSessionDelegate {
         self.last4 = last4
         self.findPassOnWatchCompletion = completion
         super.init()
-        
+
         let existingPassOnDevice: PKPass? = {
             if #available(iOS 13.4, *) {
                 return PKPassLibrary().passes(of: PKPassType.secureElement)
@@ -32,9 +32,9 @@ internal class PaymentPassFinder: NSObject, WCSessionDelegate {
                     .first(where: { $0.paymentPass?.primaryAccountNumberSuffix == last4 && $0.paymentPass?.passActivationState != .suspended && !$0.isRemotePass })
             }
         }()
-        
+
         self.isPassOnCurrentDevice = existingPassOnDevice != nil
-        
+
         if WCSession.isSupported() {
             findPassOnWatchCompletion = completion
             let session = WCSession.default
@@ -44,14 +44,14 @@ internal class PaymentPassFinder: NSObject, WCSessionDelegate {
             completion(!isPassOnCurrentDevice, isPassOnCurrentDevice ? [PassLocation.CURRENT_DEVICE] : [])
         }
     }
-    
+
     // no paired device: return pass on device (if exists), canAddPass = !ifExists
     // paired device, no pass: return pass on device, canAddPass = true
     // paried device, pass: return pass on paired device, return pass on device (if exists) canAddPass = !ifExists
-    
-    
+
+
     // START: WCSessionDelegate methods
-    
+
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
         if activationState == .activated {
             let existingPassOnPairedDevices: PKPass? = {
@@ -63,7 +63,7 @@ internal class PaymentPassFinder: NSObject, WCSessionDelegate {
                         .first(where: { $0.paymentPass?.primaryAccountNumberSuffix == last4 && $0.paymentPass?.passActivationState != .suspended })
                 }
             }()
-            
+
             var passLocations: [PassLocation] = []
             if (isPassOnCurrentDevice) {
                 passLocations.append(.CURRENT_DEVICE)
@@ -71,7 +71,7 @@ internal class PaymentPassFinder: NSObject, WCSessionDelegate {
             if (existingPassOnPairedDevices != nil) {
                 passLocations.append(.PAIRED_DEVICE)
             }
-            
+
             findPassOnWatchCompletion(
                 passLocations.count < 2,
                 passLocations
@@ -80,10 +80,10 @@ internal class PaymentPassFinder: NSObject, WCSessionDelegate {
             findPassOnWatchCompletion(!isPassOnCurrentDevice, isPassOnCurrentDevice ? [PassLocation.CURRENT_DEVICE] : [])
         }
     }
-    
+
     func sessionDidBecomeInactive(_ session: WCSession) {}
-    
+
     func sessionDidDeactivate(_ session: WCSession) {}
-    
+
     // END: WCSessionDelegate methods
 }
