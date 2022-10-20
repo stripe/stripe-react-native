8b4d5530ac29946e0eabd6580fe81f82b827004d
Perqin <perqinxie@gmail.com>
2022-10-21 00:01:58 +0800

    feat: add support of custom fonts to CardForm and CardView for Android (#1150)
    * feat: add support of custom fonts to card elements for Android
    
    * fix: remove duplicated font file
    
    * refactor: use same bindings for placeholder color and typeface

--- android/src/main/java/com/reactnativestripesdk/CardFormView.kt
+++ android/src/main/java/com/reactnativestripesdk/CardFormView.kt
@@ -12,6 +12,7 @@ import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.UIManagerModule
 import com.facebook.react.uimanager.events.EventDispatcher
+import com.facebook.react.views.text.ReactTypefaceUtils
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.ShapeAppearanceModel
@@ -134,6 +135,12 @@ class CardFormView(context: ThemedReactContext) : FrameLayout(context) {
       cardFormViewBinding.cardMultilineWidget.expiryDateEditText,
       cardFormViewBinding.postalCode
     )
+    val placeholderTextBindings = setOf(
+      multilineWidgetBinding.tlExpiry,
+      multilineWidgetBinding.tlCardNumber,
+      multilineWidgetBinding.tlCvc,
+      cardFormViewBinding.postalCodeContainer,
+    )
 
     textColor?.let {
       for (binding in editTextBindings) {
@@ -148,10 +155,9 @@ class CardFormView(context: ThemedReactContext) : FrameLayout(context) {
       }
     }
     placeholderColor?.let {
-      multilineWidgetBinding.tlExpiry.defaultHintTextColor = ColorStateList.valueOf(Color.parseColor(it))
-      multilineWidgetBinding.tlCardNumber.defaultHintTextColor = ColorStateList.valueOf(Color.parseColor(it))
-      multilineWidgetBinding.tlCvc.defaultHintTextColor = ColorStateList.valueOf(Color.parseColor(it))
-      cardFormViewBinding.postalCodeContainer.defaultHintTextColor = ColorStateList.valueOf(Color.parseColor(it))
+      for (binding in placeholderTextBindings) {
+        binding.defaultHintTextColor = ColorStateList.valueOf(Color.parseColor(it))
+      }
     }
     fontSize?.let {
       for (binding in editTextBindings) {
@@ -159,9 +165,17 @@ class CardFormView(context: ThemedReactContext) : FrameLayout(context) {
       }
     }
     fontFamily?.let {
+      // Load custom font from assets, and fallback to default system font
+      val typeface = ReactTypefaceUtils.applyStyles(null, -1, -1, it.takeIf { it.isNotEmpty() }, context.assets)
       for (binding in editTextBindings) {
-        binding.typeface = Typeface.create(it, Typeface.NORMAL)
+        binding.typeface = typeface
       }
+      for (binding in placeholderTextBindings) {
+        binding.typeface = typeface
+      }
+      cardFormViewBinding.countryLayout.typeface = typeface
+      cardFormViewBinding.countryLayout.countryAutocomplete.typeface = typeface
+      cardFormViewBinding.errors.typeface = typeface
     }
     cursorColor?.let {
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
