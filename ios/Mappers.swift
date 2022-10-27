a424af76650ade9f9e6c9f08600f87c9d8ac4715
vardges-stripe <92807969+vardges-stripe@users.noreply.github.com>
2022-10-27 10:04:07 -0700

    fix: Sets return URL for connections based on urlScheme (#1178)

--- ios/Mappers.swift
+++ ios/Mappers.swift
@@ -53,7 +53,7 @@ class Mappers {
         guard let bankAccount = bankAccount else {
             return nil
         }
-        
+
         let result: NSDictionary = [
             "id": bankAccount.stripeID,
             "bankName": bankAccount.bankName ?? NSNull(),
@@ -741,6 +741,10 @@ class Mappers {
 
     class func mapToReturnURL(urlScheme: String) -> String {
         return urlScheme + "://safepay"
+    }
+
+    class func mapToFinancialConnectionsReturnURL(urlScheme: String) -> String {
+        return urlScheme + "://financial_connections_redirect"
     }
 
     class func mapUICustomization(_ params: NSDictionary) -> STPThreeDSUICustomization {
