06daf4a0b1b680f9e75ec076ee69a7effaa4b04a
Hugo EXTRAT <extrat.h@gmail.com>
2023-02-10 12:38:34 +0100

    clean project

--- example/src/Config.ts
+++
@@ -1,8 +0,0 @@
-import { Platform } from 'react-native';
-import ENV from 'react-native-config';
-
-// Address to stripe server running on local machine
-export const LOCAL_URL =
-  Platform.OS === 'android' ? 'http://10.0.2.2:4242' : 'http://localhost:4242';
-
-export const API_URL = ENV.API_URL ? ENV.API_URL : LOCAL_URL;
