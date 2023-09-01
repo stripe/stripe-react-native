3a1e20491bf51a1f5ea2372df17c895a58c1973a
Charlie Cruzan <charliecruzan@stripe.com>
2023-09-01 09:06:05 -1000

    better example

--- example/src/screens/CustomerSheetScreen.tsx
+++ example/src/screens/CustomerSheetScreen.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Alert, Image, StyleSheet } from 'react-native';
+import { Alert, Image, StyleSheet, Switch, Text, View } from 'react-native';
 import { CustomerSheetBeta } from '@stripe/stripe-react-native';
 import type { PaymentSheet } from '@stripe/stripe-react-native';
 import Button from '../components/Button';
 import PaymentScreen from '../components/PaymentScreen';
 import { API_URL } from '../Config';
 
-const { CustomerSheet } = CustomerSheetBeta;
+const {
+  CustomerSheet,
+  initCustomerSheet,
+  presentCustomerSheet,
+  retrieveCustomerSheetPaymentOptionSelection,
+} = CustomerSheetBeta;
 
 export default function CustomerSheetScreen() {
+  const [useComponent, setUseComponent] = React.useState(false);
   const [stripeInitialized, setStripeInitialized] = React.useState(false);
-  const [ready, setReady] = React.useState(false);
+  const [selectedPaymentOption, setSelectedPaymentOption] =
+    React.useState<PaymentSheet.PaymentOption | null>(null);
   const [setupIntent, setSetupIntent] = React.useState('');
   const [ephemeralKeySecret, setEphemeralKeySecret] = React.useState('');
   const [customer, setCustomer] = React.useState('');
   const [customerSheetVisible, setCustomerSheetVisible] = React.useState(false);
-  const [selectedPaymentOption, setSelectedPaymentOption] =
-    React.useState<PaymentSheet.PaymentOption | null>(null);
-
-  React.useEffect(() => {
-    fetchCustomerSheetParams();
-  }, []);
 
   const fetchCustomerSheetParams = async () => {
     const response = await fetch(`${API_URL}/customer-sheet`, {
@@ -37,29 +38,105 @@ export default function CustomerSheetScreen() {
       setSetupIntent(result.setupIntent);
       setEphemeralKeySecret(result.ephemeralKeySecret);
       setCustomer(result.customer);
-      setReady(true);
+    }
+
+    return {
+      setupIntent: result.setupIntent,
+      ephemeralKeySecret: result.ephemeralKeySecret,
+      customer: result.customer,
+    };
+  };
+
+  const setup = async () => {
+    const {
+      customer: customerId,
+      setupIntent: setupIntentClientSecret,
+      ephemeralKeySecret: customerEphemeralKeySecret,
+    } = await fetchCustomerSheetParams();
+    const { error } = await initCustomerSheet({
+      setupIntentClientSecret,
+      customerEphemeralKeySecret,
+      customerId,
+      returnURL: 'stripe-example://stripe-redirect',
+    });
+    if (error) {
+      Alert.alert(error.code, error.localizedMessage);
+    }
+
+    const {
+      error: retrievalError,
+      paymentOption,
+      paymentMethod,
+    } = await retrieveCustomerSheetPaymentOptionSelection();
+    if (retrievalError) {
+      Alert.alert(retrievalError.code, retrievalError.localizedMessage);
+    }
+    if (paymentOption) {
+      setSelectedPaymentOption(paymentOption);
+      console.log(JSON.stringify(paymentOption, null, 2));
+    }
+    if (paymentMethod) {
+      console.log(JSON.stringify(paymentMethod, null, 2));
+    }
+
+    setStripeInitialized(true);
+  };
+
+  const present = async () => {
+    if (useComponent) {
+      setCustomerSheetVisible(true);
+    } else {
+      const { error, paymentOption, paymentMethod } =
+        await presentCustomerSheet();
+      if (error) {
+        Alert.alert(error.code, error.localizedMessage);
+      }
+      if (paymentOption) {
+        setSelectedPaymentOption(paymentOption);
+        console.log(JSON.stringify(paymentOption, null, 2));
+      }
+      if (paymentMethod) {
+        console.log(JSON.stringify(paymentMethod, null, 2));
+      }
     }
   };
 
   return (
-    <PaymentScreen
-      onInit={() => {
-        setStripeInitialized(true);
-      }}
-    >
+    <PaymentScreen onInit={setup}>
       <Button
         variant="primary"
         loading={!stripeInitialized}
-        disabled={!ready}
         title={
           selectedPaymentOption
             ? selectedPaymentOption.label
             : 'Edit payment methods'
         }
         onPress={() => {
-          setCustomerSheetVisible(true);
+          present();
         }}
       />
+      {useComponent && (
+        <CustomerSheet
+          visible={customerSheetVisible}
+          setupIntentClientSecret={setupIntent}
+          customerEphemeralKeySecret={ephemeralKeySecret}
+          customerId={customer}
+          returnURL={'stripe-example://stripe-redirect'}
+          onResult={({ error, paymentOption, paymentMethod }) => {
+            setCustomerSheetVisible(false);
+            if (error) {
+              Alert.alert(error.code, error.localizedMessage);
+            }
+            if (paymentOption) {
+              setSelectedPaymentOption(paymentOption);
+              console.log(JSON.stringify(paymentOption, null, 2));
+            }
+            if (paymentMethod) {
+              console.log(JSON.stringify(paymentMethod, null, 2));
+            }
+          }}
+        />
+      )}
       {selectedPaymentOption?.image && (
         <Image
           style={styles.image}
@@ -68,30 +145,23 @@ export default function CustomerSheetScreen() {
           }}
         />
       )}
-      <CustomerSheet
-        visible={customerSheetVisible}
-        setupIntentClientSecret={setupIntent}
-        customerEphemeralKeySecret={ephemeralKeySecret}
-        customerId={customer}
-        returnURL={'stripe-example://stripe-redirect'}
-        onResult={({ error, paymentOption, paymentMethod }) => {
-          setCustomerSheetVisible(false);
-          if (error) {
-            Alert.alert(error.code, error.localizedMessage);
-          }
-          if (paymentOption) {
-            setSelectedPaymentOption(paymentOption);
-            console.log(JSON.stringify(paymentOption, null, 2));
-          }
-          if (paymentMethod) {
-            console.log(JSON.stringify(paymentMethod, null, 2));
-          }
-        }}
-      />
+      <View style={styles.switchRow}>
+        <Text style={styles.switchLabel}>Use component: </Text>
+        <Switch onValueChange={setUseComponent} value={useComponent} />
+      </View>
     </PaymentScreen>
   );
 }
 
 const styles = StyleSheet.create({
+  switchRow: {
+    marginTop: 350,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  switchLabel: {
+    fontSize: 20,
+  },
   image: { alignSelf: 'center', width: 150, height: 100 },
 });
