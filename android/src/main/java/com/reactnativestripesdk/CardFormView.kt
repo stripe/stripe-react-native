facab1ae14e417c81c707fabfe706920de2f1361
Carlos Molina <57185359+Garlondrake@users.noreply.github.com>
2024-01-30 18:18:14 +0100

    fix: Lint problems that make lint analysis fail and not completing the build (#1600)

--- android/src/main/java/com/reactnativestripesdk/CardFormView.kt
+++ android/src/main/java/com/reactnativestripesdk/CardFormView.kt
@@ -1,5 +1,6 @@
 package com.reactnativestripesdk
 
+import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Build
@@ -62,6 +63,7 @@ class CardFormView(context: ThemedReactContext) : FrameLayout(context) {
     cardForm.isEnabled = !isDisabled
   }
 
+  @SuppressLint("RestrictedApi")
   private fun setCountry(countryString: String?) {
     if (countryString != null) {
       cardFormViewBinding.countryLayout.setSelectedCountryCode(CountryCode(countryString))
@@ -122,6 +124,7 @@ class CardFormView(context: ThemedReactContext) : FrameLayout(context) {
       CardFocusEvent(id, currentFocusedField))
   }
 
+  @SuppressLint("RestrictedApi")
   fun setCardStyle(value: ReadableMap) {
     val backgroundColor = getValOr(value, "backgroundColor", null)
     val textColor = getValOr(value, "textColor", null)
@@ -287,6 +290,7 @@ class CardFormView(context: ThemedReactContext) : FrameLayout(context) {
     )
   }
 
+  @SuppressLint("RestrictedApi")
   private fun createPostalCodeInputFilter(): InputFilter {
     return InputFilter { charSequence, start, end, _, _, _ ->
       if (cardFormViewBinding.countryLayout.getSelectedCountryCode() == CountryCode.US) {
