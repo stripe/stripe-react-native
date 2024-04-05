213090470e0d2a09fd505c8254e4b10329a21164
David Goguen <davidgoguen@iroomit.com>
2024-04-05 16:13:35 -0400

    update wording in error to reflect underscores being allowed

--- android/src/main/java/com/reactnativestripesdk/PaymentSheetAppearance.kt
+++ android/src/main/java/com/reactnativestripesdk/PaymentSheetAppearance.kt
@@ -163,7 +163,7 @@ private fun getFontResId(bundle: Bundle?, key: String, defaultValue: Int?, conte
           ?: throw PaymentSheetAppearanceException("$fontErrorPrefix expected String for font.$key, but received null.")
   if (Regex("[^a-z0-9_]").containsMatchIn(fontFileName)) {
     throw PaymentSheetAppearanceException(
-      "$fontErrorPrefix appearance.font.$key should only contain lowercase alphanumeric characters on Android, but received '$fontFileName'. This value must match the filename in android/app/src/main/res/font"
+      "$fontErrorPrefix appearance.font.$key should only contain lowercase alphanumeric characters and underscores on Android, but received '$fontFileName'. This value must match the filename in android/app/src/main/res/font"
     )
   }
 
