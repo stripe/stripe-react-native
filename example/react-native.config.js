272849365aa4dd5df7202882493e714cecccd8a9
Janic Duplessis <janicduplessis@gmail.com>
2025-02-27 01:21:21 -0500

    Add rnt package to configure android build

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const project = (() => {
   try {
     const { configureProjects } = require('react-native-test-app');
@@ -14,6 +16,18 @@ const project = (() => {
   }
 })();
 
+const rntPackageRoot = path.join(__dirname, 'rnt-package');
+
 module.exports = {
   ...(project ? { project } : undefined),
+  dependencies: {
+    'rnt-package': {
+      root: rntPackageRoot,
+      platforms: {
+        android: {
+          sourceDir: path.join(rntPackageRoot, 'android'),
+        },
+      },
+    },
+  },
 };
