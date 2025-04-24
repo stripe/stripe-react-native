cb762064b857317caec8747e401acb0b6edf97ee
Nick Porter <88012362+porter-stripe@users.noreply.github.com>
2025-04-24 10:40:46 -0600

    Implement embedded for iOS (#1898)
    * Implement embedded for iOS
    
    * remove update payment method flag
    
    * Remove dead code
    
    * Update StripeSdk.m
    
    * Resolve warnings
    
    * Use leading and trailing anchors
    
    * Rename to buildEmbeddedPaymentElementAppearance
    
    * Rename to embeddedAppearance
    
    * Update error messages
    
    * Small nits
    
    * Simplify buildEdgeInsets

---
+++ ios/EmbeddedPaymentElementView.swift
@@ -0,0 +1,51 @@
+//
+//  EmbeddedPaymentElementView.swift
+//  stripe-react-native
+//
+//  Created by Nick Porter on 4/16/25.
+//
+
+import Foundation
+import UIKit
+@_spi(EmbeddedPaymentElementPrivateBeta) import StripePaymentSheet
+
+@objc(EmbeddedPaymentElementView)
+class EmbeddedPaymentElementView: RCTViewManager {
+  
+  override static func requiresMainQueueSetup() -> Bool {
+    return true
+  }
+  
+  override func view() -> UIView! {
+    return EmbeddedPaymentElementContainerView(frame: .zero, stripeSdk: bridge.module(forName: "StripeSdk") as! StripeSdk)
+  }
+}
+
+class EmbeddedPaymentElementContainerView: UIView, UIGestureRecognizerDelegate {
+  private let stripeSdk: StripeSdk
+
+  init(frame: CGRect, stripeSdk: StripeSdk) {
+    self.stripeSdk = stripeSdk
+    super.init(frame: frame)
+    backgroundColor = .clear
+    attachPaymentElementIfAvailable()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+
+  private func attachPaymentElementIfAvailable() {
+    guard let embeddedElement = stripeSdk.embeddedInstance else { return }
+    let paymentElementView = embeddedElement.view
+    addSubview(paymentElementView)
+    paymentElementView.translatesAutoresizingMaskIntoConstraints = false
+
+    NSLayoutConstraint.activate([
+      paymentElementView.topAnchor.constraint(equalTo: topAnchor),
+      paymentElementView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      paymentElementView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      paymentElementView.bottomAnchor.constraint(equalTo: bottomAnchor),
+    ])
+  }
+}
--- ios/StripeSdk+PaymentSheet.swift
+++ ios/StripeSdk+PaymentSheet.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-@_spi(ExperimentalAllowsRemovalOfLastSavedPaymentMethodAPI) @_spi(CustomerSessionBetaAccess) @_spi(STP) import StripePaymentSheet
+@_spi(ExperimentalAllowsRemovalOfLastSavedPaymentMethodAPI) @_spi(CustomerSessionBetaAccess) @_spi(EmbeddedPaymentElementPrivateBeta) @_spi(STP) import StripePaymentSheet
 
 extension StripeSdk {
     internal func buildPaymentSheetConfiguration(
@@ -268,7 +268,7 @@ extension StripeSdk {
         }
     }
 
-    private func mapCaptureMethod(_ captureMethod: String?) -> PaymentSheet.IntentConfiguration.CaptureMethod {
+    func mapCaptureMethod(_ captureMethod: String?) -> PaymentSheet.IntentConfiguration.CaptureMethod {
         if let captureMethod = captureMethod {
             switch captureMethod {
             case "Automatic": return PaymentSheet.IntentConfiguration.CaptureMethod.automatic
@@ -280,7 +280,7 @@ extension StripeSdk {
         return PaymentSheet.IntentConfiguration.CaptureMethod.automatic
     }
 
-    private func buildIntentConfiguration(
+    func buildIntentConfiguration(
         modeParams: NSDictionary,
         paymentMethodTypes: [String]?,
         captureMethod: PaymentSheet.IntentConfiguration.CaptureMethod
@@ -318,7 +318,7 @@ extension StripeSdk {
             })
     }
 
-    private func buildCustomerHandlersForPaymentSheet(applePayParams: NSDictionary) -> PaymentSheet.ApplePayConfiguration.Handlers? {
+    func buildCustomerHandlersForPaymentSheet(applePayParams: NSDictionary) -> PaymentSheet.ApplePayConfiguration.Handlers? {
         if (applePayParams["request"] == nil) {
             return nil
         }
