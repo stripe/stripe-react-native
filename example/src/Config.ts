f4c2c320beb56415ae9e832409cb12bd5d790187
tianzhao-stripe <tianzhao@stripe.com>
2025-06-24 18:14:00 -0700

    new codesandbox url

--- example/src/Config.ts
+++ example/src/Config.ts
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 // Address to stripe server running on local machine
 const LOCAL_URL =
   Platform.OS === 'android' ? 'http://10.0.2.2:4242' : 'http://localhost:4242';
-// Address to stripe server running on Glitch
-const GLITCH_URL = 'https://rigorous-heartbreaking-cephalopod.glitch.me';
+// Address to stripe server running on CodeSandBox
+const CODESANDBOX_URL = 'https://m358cz-3000.csb.app';
 
-export const API_URL = __DEV__ ? LOCAL_URL : GLITCH_URL;
+export const API_URL = __DEV__ ? LOCAL_URL : CODESANDBOX_URL;
