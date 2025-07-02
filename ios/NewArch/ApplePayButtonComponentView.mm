7d90d858821fd47a85c38f270f4bc177660f8203
Joyce Qin <joyceqin@stripe.com>
2025-07-02 14:23:17 -0700

    simplified

--- ios/NewArch/ApplePayButtonComponentView.mm
+++ ios/NewArch/ApplePayButtonComponentView.mm
@@ -41,6 +41,51 @@ using namespace facebook::react;
 {
   _view = [[ApplePayButtonView alloc] initWithFrame:self.frame];
   self.contentView = _view;
+
+  __weak __typeof(self) weakSelf = self;
+
+  _view.onShippingMethodSelectedAction = ^(NSDictionary *event) {
+    __typeof(self) strongSelf = weakSelf;
+    if (!strongSelf || !strongSelf->_eventEmitter) {
+      return;
+    }
+    ApplePayButtonEventEmitter::OnShippingMethodSelectedAction emitterEvent = {
+        .shippingMethod = convertIdToFollyDynamic(event[@"shippingMethod"])};
+    std::static_pointer_cast<const ApplePayButtonEventEmitter>(strongSelf->_eventEmitter)
+        ->onShippingMethodSelectedAction(std::move(emitterEvent));
+  };
+
+  _view.onShippingContactSelectedAction = ^(NSDictionary *event) {
+    __typeof(self) strongSelf = weakSelf;
+    if (!strongSelf || !strongSelf->_eventEmitter) {
+      return;
+    }
+    ApplePayButtonEventEmitter::OnShippingContactSelectedAction emitterEvent = {
+        .shippingContact = convertIdToFollyDynamic(event[@"shippingContact"])};
+    std::static_pointer_cast<const ApplePayButtonEventEmitter>(strongSelf->_eventEmitter)
+        ->onShippingContactSelectedAction(std::move(emitterEvent));
+  };
+
+  _view.onCouponCodeEnteredAction = ^(NSDictionary *event) {
+    __typeof(self) strongSelf = weakSelf;
+    if (!strongSelf || !strongSelf->_eventEmitter) {
+      return;
+    }
+    ApplePayButtonEventEmitter::OnCouponCodeEnteredAction emitterEvent = {
+        .couponCode = RCTStringFromNSString(event[@"couponCode"])};
+    std::static_pointer_cast<const ApplePayButtonEventEmitter>(strongSelf->_eventEmitter)
+        ->onCouponCodeEnteredAction(std::move(emitterEvent));
+  };
+
+  _view.onOrderTrackingAction = ^(NSDictionary *event) {
+    __typeof(self) strongSelf = weakSelf;
+    if (!strongSelf || !strongSelf->_eventEmitter) {
+      return;
+    }
+    ApplePayButtonEventEmitter::OnOrderTrackingAction emitterEvent = {};
+    std::static_pointer_cast<const ApplePayButtonEventEmitter>(strongSelf->_eventEmitter)
+        ->onOrderTrackingAction(std::move(emitterEvent));
+  };
 }
 
 - (void)updateProps:(const facebook::react::Props::Shared &)props
@@ -52,76 +97,14 @@ using namespace facebook::react;
   _view.buttonStyle = @(newViewProps.buttonStyle);
   _view.disabled = newViewProps.disabled;
   _view.borderRadius = @(newViewProps.borderRadius);
-  
+
   // Set the boolean flags from props
-  _view.hasShippingMethodCallback = newViewProps.hasShippingMethodCallback;
-  _view.hasShippingContactCallback = newViewProps.hasShippingContactCallback;
-  _view.hasCouponCodeCallback = newViewProps.hasCouponCodeCallback;
-  _view.hasOrderTrackingCallback = newViewProps.hasOrderTrackingCallback;
+  _view.hasShippingMethodCallback = @(newViewProps.hasShippingMethodCallback);
+  _view.hasShippingContactCallback = @(newViewProps.hasShippingContactCallback);
+  _view.hasCouponCodeCallback = @(newViewProps.hasCouponCodeCallback);
+  _view.hasOrderTrackingCallback = @(newViewProps.hasOrderTrackingCallback);
 
   [super updateProps:props oldProps:oldProps];
-
-  // Set up callbacks conditionally based on the boolean flags
-  __weak __typeof(self) weakSelf = self;
-  
-  if (newViewProps.hasShippingMethodCallback) {
-    _view.onShippingMethodSelectedAction = ^(NSDictionary *event) {
-      __typeof(self) strongSelf = weakSelf;
-      if (!strongSelf || !strongSelf->_eventEmitter) {
-        return;
-      }
-      ApplePayButtonEventEmitter::OnShippingMethodSelectedAction emitterEvent = {
-          .shippingMethod = convertIdToFollyDynamic(event[@"shippingMethod"])};
-      std::static_pointer_cast<const ApplePayButtonEventEmitter>(strongSelf->_eventEmitter)
-          ->onShippingMethodSelectedAction(std::move(emitterEvent));
-    };
-  } else {
-    _view.onShippingMethodSelectedAction = nil;
-  }
-
-  if (newViewProps.hasShippingContactCallback) {
-    _view.onShippingContactSelectedAction = ^(NSDictionary *event) {
-      __typeof(self) strongSelf = weakSelf;
-      if (!strongSelf || !strongSelf->_eventEmitter) {
-        return;
-      }
-      ApplePayButtonEventEmitter::OnShippingContactSelectedAction emitterEvent = {
-          .shippingContact = convertIdToFollyDynamic(event[@"shippingContact"])};
-      std::static_pointer_cast<const ApplePayButtonEventEmitter>(strongSelf->_eventEmitter)
-          ->onShippingContactSelectedAction(std::move(emitterEvent));
-    };
-  } else {
-    _view.onShippingContactSelectedAction = nil;
-  }
-
-  if (newViewProps.hasCouponCodeCallback) {
-    _view.onCouponCodeEnteredAction = ^(NSDictionary *event) {
-      __typeof(self) strongSelf = weakSelf;
-      if (!strongSelf || !strongSelf->_eventEmitter) {
-        return;
-      }
-      ApplePayButtonEventEmitter::OnCouponCodeEnteredAction emitterEvent = {
-          .couponCode = RCTStringFromNSString(event[@"couponCode"])};
-      std::static_pointer_cast<const ApplePayButtonEventEmitter>(strongSelf->_eventEmitter)
-          ->onCouponCodeEnteredAction(std::move(emitterEvent));
-    };
-  } else {
-    _view.onCouponCodeEnteredAction = nil;
-  }
-
-  if (newViewProps.hasOrderTrackingCallback) {
-    _view.onOrderTrackingAction = ^(NSDictionary *event) {
-      __typeof(self) strongSelf = weakSelf;
-      if (!strongSelf || !strongSelf->_eventEmitter) {
-        return;
-      }
-      ApplePayButtonEventEmitter::OnOrderTrackingAction emitterEvent = {};
-      std::static_pointer_cast<const ApplePayButtonEventEmitter>(strongSelf->_eventEmitter)
-          ->onOrderTrackingAction(std::move(emitterEvent));
-    };
-  } else {
-    _view.onOrderTrackingAction = nil;
-  }
 
   [_view didSetProps];
 }
