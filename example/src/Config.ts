ce5e672416b4063bf8c0c596b0ad87dd8ed9839e
tianzhao-stripe <tianzhao@stripe.com>
2025-07-03 11:39:26 -0700

    update urls

--- example/src/Config.ts
+++ example/src/Config.ts
@@ -3,7 +3,8 @@ import { Platform } from 'react-native';
 // Address to stripe server running on local machine
 const LOCAL_URL =
   Platform.OS === 'android' ? 'http://10.0.2.2:4242' : 'http://localhost:4242';
-// Address to stripe server running on CodeSandBox
-const CODESANDBOX_URL = 'https://m358cz-3000.csb.app';
+// Heres a link to a codesandbox you can fork and edit for yourself https://codesandbox.io/p/devbox/rigorous-heartbreaking-cephalopod-m358cz;
+const DEMO_BACKEND_URL =
+  'http://rigorous-heartbreaking-cephalopod.stripedemos.com';
 
-export const API_URL = __DEV__ ? LOCAL_URL : CODESANDBOX_URL;
+export const API_URL = __DEV__ ? LOCAL_URL : DEMO_BACKEND_URL;
