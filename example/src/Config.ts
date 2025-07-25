4a44822a0009c3a685201127251a89db42f20faf
Nick Porter <porter@stripe.com>
2025-07-25 11:48:14 -0600

    Re-add stub for local server

--- example/src/Config.ts
+++ example/src/Config.ts
@@ -1,3 +1,10 @@
+// import { Platform } from 'react-native';
+
+// // Address to stripe server running on local machine
+// const LOCAL_URL =
+//   Platform.OS === 'android' ? 'http://10.0.2.2:4242' : 'http://localhost:4242';
+// // Heres a link to a codesandbox you can fork and edit for yourself https://codesandbox.io/p/devbox/rigorous-heartbreaking-cephalopod-m358cz;
+
 // Demo backend URL - no local server setup required
 const DEMO_BACKEND_URL =
   'https://rigorous-heartbreaking-cephalopod.stripedemos.com';
