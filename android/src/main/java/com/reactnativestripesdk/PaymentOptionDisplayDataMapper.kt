6a683043ff063456d55d94188be95fbd3b52d021
Nick Porter <88012362+porter-stripe@users.noreply.github.com>
2025-08-18 14:13:55 -0600

    Add image to embedded payment option display data (#2032)
    * Add image to embedded payment option display data
    
    * Simplify implementation with new Android version
    
    * Lint
    
    * Empty commit to trigger CI

--- android/src/main/java/com/reactnativestripesdk/PaymentOptionDisplayDataMapper.kt
+++ android/src/main/java/com/reactnativestripesdk/PaymentOptionDisplayDataMapper.kt
@@ -1,15 +1,40 @@
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.WritableMap
+import com.reactnativestripesdk.getBase64FromBitmap
+import com.reactnativestripesdk.getBitmapFromDrawable
 import com.reactnativestripesdk.utils.mapFromPaymentSheetBillingDetails
 import com.stripe.android.paymentelement.EmbeddedPaymentElement
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 
 /**
  * Serialize Stripe's PaymentOptionDisplayData into a WritableMap
  * that can be sent over the RN bridge.
  */
-fun EmbeddedPaymentElement.PaymentOptionDisplayData.toWritableMap(): WritableMap =
+suspend fun EmbeddedPaymentElement.PaymentOptionDisplayData.toWritableMap(): WritableMap =
   Arguments.createMap().apply {
     putString("label", label)
     putString("paymentMethodType", paymentMethodType)
     putMap("billingDetails", mapFromPaymentSheetBillingDetails(billingDetails))
+
+    // Load image off the main thread with a timeout
+    val imageBase64 =
+      try {
+        withContext(Dispatchers.Default) {
+          val drawable =
+            withTimeout(5_000L) {
+              withContext(Dispatchers.IO) {
+                imageLoader()
+              }
+            }
+          getBitmapFromDrawable(drawable)?.let { bitmap ->
+            getBase64FromBitmap(bitmap)
+          } ?: ""
+        }
+      } catch (e: Exception) {
+        // If imageLoader fails or times out, return empty string
+        ""
+      }
+    putString("image", imageBase64)
   }
