d4c22e79fa4fd0b572f9290a8ac8e99efa32d595
Twig <aharris.1990@gmail.com>
2025-09-10 14:47:10 -0400

    Move function into utils

--- example/src/screens/Onramp/sections/RegisterWalletAddressSection.tsx
+++ example/src/screens/Onramp/sections/RegisterWalletAddressSection.tsx
@@ -5,6 +5,7 @@ import { Collapse } from '../../../components/Collapse';
 import Button from '../../../components/Button';
 import { FormField } from '../FormField';
 import { useOnramp, Onramp } from '@stripe/stripe-react-native';
+import { getDefaultAddressForNetwork } from '../utils';
 
 interface RegisterWalletAddressSectionProps {
   onWalletRegistered?: (address: string, network: Onramp.CryptoNetwork) => void;
@@ -18,34 +19,6 @@ export function RegisterWalletAddressSection({
     Onramp.CryptoNetwork.ethereum
   );
 
-  // Sample addresses for different networks
-  const getDefaultAddressForNetwork = useCallback(
-    (cryptoNetwork: Onramp.CryptoNetwork): string => {
-      switch (cryptoNetwork) {
-        case Onramp.CryptoNetwork.ethereum:
-        case Onramp.CryptoNetwork.polygon:
-        case Onramp.CryptoNetwork.avalanche:
-        case Onramp.CryptoNetwork.base:
-        case Onramp.CryptoNetwork.optimism:
-        case Onramp.CryptoNetwork.worldchain:
-          return '0x742d35Cc6634C0532925a3b844Bc454e4438f44e';
-        case Onramp.CryptoNetwork.solana:
-          return '9WzDXwBbmkg8ZTbNMqUxvQRAyrZzDsGYdLVL9zYtAWWM';
-        case Onramp.CryptoNetwork.bitcoin:
-          return '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa';
-        case Onramp.CryptoNetwork.stellar:
-          return 'GDQP2KPQGKIHYJGXNUIYOMHARUARCA7DJT5FO2FFOOKY3B2WSQHG4W37';
-        case Onramp.CryptoNetwork.aptos:
-          return '0x1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c8d9e0f1a2b';
-        case Onramp.CryptoNetwork.xrpl:
-          return 'rN7n7otQDd6FczFgLdSqtcsAUxDkw6fzRH';
-        default:
-          return '0x742d35Cc6634C0532925a3b844Bc454e4438f44e';
-      }
-    },
-    []
-  );
-
   const [walletAddress, setWalletAddress] = useState(
     getDefaultAddressForNetwork(Onramp.CryptoNetwork.ethereum)
   );
@@ -57,7 +30,7 @@ export function RegisterWalletAddressSection({
       setNetwork(newNetwork);
       setWalletAddress(getDefaultAddressForNetwork(newNetwork));
     },
-    [getDefaultAddressForNetwork]
+    []
   );
 
   const handleRegisterWallet = useCallback(async () => {
