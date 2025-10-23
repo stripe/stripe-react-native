5a34b5545f4f153ad87ecc85655f7ea78082142c
Alberto Gimeno <gimenete@stripe.com>
2025-10-23 10:47:47 +0200

    Fake publishable key

--- example/src/helpers.ts
+++ example/src/helpers.ts
@@ -1,10 +1,15 @@
 import { Alert } from 'react-native';
 import { API_URL } from './Config';
 
+const fakePublishableKey = true;
+
 export async function fetchPublishableKey(
   paymentMethod?: string
 ): Promise<string | null> {
   try {
+    if (fakePublishableKey) {
+      return 'uk_TEZmVlG97Bvkhi3hTJjI1or06NO970Ra00QZ7nWQ3i';
+    }
     const response = await fetch(
       `${API_URL}/stripe-key?paymentMethod=${paymentMethod}`
     );
