76fc8b726386a1a80e8af33dbdefbd0d8c8c26d1
Tyler Clawson <tjclawson@stripe.com>
2026-02-10 13:12:32 -0500

    Update

--- ios/OldArch/StripeSdkEventEmitterCompat.m
+++ ios/OldArch/StripeSdkEventEmitterCompat.m
@@ -25,7 +25,6 @@
     @"embeddedPaymentElementLoadingFailed",
     @"onCustomPaymentMethodConfirmHandlerCallback",
     @"onCheckoutClientSecretRequested",
-    @"onCheckoutClientSecretRequested",
     @"paymentMethodMessagingElementDidUpdateHeight",
     @"paymentMethodMessagingElementConfigureResult",
   ];
