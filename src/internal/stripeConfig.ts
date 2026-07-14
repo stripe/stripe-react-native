3c3757c3fe4d59324d41f67cf42649ec25bc9620
Twig <aharris.1990@gmail.com>
2026-07-14 14:40:11 -0400

    Remove last part of the shim

--- src/internal/stripeConfig.ts
+++
@@ -1,9 +0,0 @@
-let currentPublishableKey: string | undefined;
-
-export function setCurrentPublishableKey(publishableKey: string): void {
-  currentPublishableKey = publishableKey;
-}
-
-export function getCurrentPublishableKey(): string | undefined {
-  return currentPublishableKey;
-}
